66ff4f0219347b9fc059c72ad5994e31a1ff464f
jaquelinemendanhasac <sacjaquelinemendanha@gmail.com>
2026-07-07 23:20:24 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = "studio-sync-pro";
-const CACHE_VERSION = "v86-auditoria";
+const CACHE_VERSION = "v87-firebase";
 const CACHE_NAME = `${CACHE_PREFIX}-${CACHE_VERSION}`;
 const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./firebase.js", "./manifest.json", "./icon-192.png", "./icon-512.png", "./maskable-192.png", "./maskable-512.png"];
 
